44c405d75277396ae083d86e128c62806af0c383
ibnaleem <ibnaleem@outlook.com>
2024-02-18 11:35:35 +0000

    lunh -> luhn typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="Luhn Algorithm",
-    version="1.0.0",
+    version="1.0.2",
     author="Ibn Aleem",
     author_email="ibnaleem@outlook.com",
     description="Python implementation of Luhn's algorithm for validating credit card numbers.",
